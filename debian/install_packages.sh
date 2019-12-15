e73c45a54ebfd3db55a4e3f46b8d5305e7190676
dlapets <lapets@adjust.com>
2019-12-15 11:19:00 -0500

    Add list of debian packages to install

---
+++ debian/install_packages.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+apt-get update
+apt-get upgrade
+apt-get install make build-essential git zsh tmux vim-nox
+apt-get install openbox ttf-mscorefonts-installer firefox-esr rxvt-unicode xfonts-terminus fonts-roboto xserver-xorg arandr
+
+#apt-get install firmware-amd-graphics amd64-microcode 
