8f4978caa5301847ec304b291589a8ea92cff83d
dlapets <lapets@adjust.com>
2019-12-15 20:31:51 -0500

    You forgot jq and/or Poland

--- debian/install_packages.sh
+++ debian/install_packages.sh
@@ -1,48 +1,63 @@
 #!/bin/bash
-apt-get update
-apt-get upgrade
+#apt-get update
+#apt-get upgrade
 
+DEBIAN_FRONTEND=noninteractive
+INSTALL="apt-get -yq install"
 
-echo "Install cli tools"
-apt-get install \
-    apt-get install \
-    build-essential \
-    fortune-mod \
-    git \
-    make \
-    psmisc \
-    silversearcher-ag \
-    sudo \
-    tmux \
-    vim-nox \
-    zsh \
+echo "Install cli tools?"
+read CHK
+if [[ "$CHK" == "y" ]]; then 
+    $INSTALL \
+        build-essential \
+        fortune-mod \
+        git \
+        jq \
+        make \
+        psmisc \
+        silversearcher-ag \
+        sudo \
+        tmux \
+        vim-nox \
+        zsh \
+        ;
+fi
 
+echo "Install X11 and friends?"
+read CHK
+if [[ "$CHK" == "y" ]]; then 
+    $INSTALL \
+        arandr \
+        firefox-esr \
+        fonts-roboto \
+        notification-daemon \
+        openbox \
+        rxvt-unicode \
+        ttf-mscorefonts-installer \
+        x11-common \
+        x11-session-utils \
+        x11-utils \
+        xclip \
+        xfonts-terminus \
+        xserver-xorg \
+        ;
+fi
 
-echo "Install kernel headers"
-apt-get install \
-    linux-headers-amd64 \
+echo "Install linux kernel headers?"
+read CHK
+if [[ "$CHK" == "y" ]]; then 
+    $INSTALL \
+        linux-headers-amd64 \
+        ;
+fi
 
-
-echo "Install X11 and friends"
-apt-get install \
-    arandr \
-    firefox-esr \
-    fonts-roboto \
-    notification-daemon \
-    openbox \
-    rxvt-unicode \
-    ttf-mscorefonts-installer \
-    x11-common \
-    x11-session-utils \
-    x11-utils \
-    xclip \
-    xfonts-terminus \
-    xserver-xorg \
-
-
-echo "Install misc. firmware/drivers"
-apt-get install \
-    amd64-microcode \
-    firmware-amd-graphics \
-    firmware-linux-nonfree \
-    firmware-realtek \
+echo "Install misc. firmware/drivers?"
+read CHK
+if [[ "$CHK" == "y" ]]; then 
+    $INSTALL \
+        amd64-microcode \
+        firmware-amd-graphics \
+        firmware-linux-nonfree \
+        firmware-realtek \
+        ;
+fi
