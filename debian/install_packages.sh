fabf7eb6b846aaf6d9354f7f9fd2986d3436078d
dlapets <lapets@adjust.com>
2019-12-15 20:06:12 -0500

    Update deb package list
    ... probably this should be organized in a nice way so that we wouldn't
    be installing random shit on any random trashbag of a computer.

--- debian/install_packages.sh
+++ debian/install_packages.sh
@@ -1,7 +1,48 @@
 #!/bin/bash
 apt-get update
 apt-get upgrade
-apt-get install make build-essential git zsh tmux vim-nox
-apt-get install openbox ttf-mscorefonts-installer firefox-esr rxvt-unicode xfonts-terminus fonts-roboto xserver-xorg arandr
 
-#apt-get install firmware-amd-graphics amd64-microcode 
+
+echo "Install cli tools"
+apt-get install \
+    apt-get install \
+    build-essential \
+    fortune-mod \
+    git \
+    make \
+    psmisc \
+    silversearcher-ag \
+    sudo \
+    tmux \
+    vim-nox \
+    zsh \
+
+
+echo "Install kernel headers"
+apt-get install \
+    linux-headers-amd64 \
+
+
+echo "Install X11 and friends"
+apt-get install \
+    arandr \
+    firefox-esr \
+    fonts-roboto \
+    notification-daemon \
+    openbox \
+    rxvt-unicode \
+    ttf-mscorefonts-installer \
+    x11-common \
+    x11-session-utils \
+    x11-utils \
+    xclip \
+    xfonts-terminus \
+    xserver-xorg \
+
+
+echo "Install misc. firmware/drivers"
+apt-get install \
+    amd64-microcode \
+    firmware-amd-graphics \
+    firmware-linux-nonfree \
+    firmware-realtek \
